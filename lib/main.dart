10d6cbb595b3b26583550fac5d0264b88d474eeb
Ali Hyder <aalihyder93@gmail.com>
2025-10-29 14:45:09 +0500

    fresh

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ class HunarHubApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Hunar Hub',
       theme: ThemeData(
-        scaffoldBackgroundColor: const Color(0xFFF7F1E7),
         textTheme: GoogleFonts.poppinsTextTheme(),
         
         useMaterial3: true,
