35e8c41e22d72db4f3416c66249fea6bdaa2d9b6
Ali Hyder <aalihyder93@gmail.com>
2025-10-29 14:31:28 +0500

    fresh

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'routes.dart';
 
 void main() {
   runApp(const HunarHubApp());
@@ -15,15 +14,10 @@ class HunarHubApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Hunar Hub',
-      initialRoute: AppRoutes.onboarding,
-      getPages: AppPages.pages,
       theme: ThemeData(
         scaffoldBackgroundColor: const Color(0xFFF7F1E7),
         textTheme: GoogleFonts.poppinsTextTheme(),
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF2AB1A6),
-          background: const Color(0xFFF7F1E7),
-        ),
+        
         useMaterial3: true,
       ),
     );
