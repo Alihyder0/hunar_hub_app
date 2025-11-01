5a9f5c4f1283cdca8773d42a8fe65624ff6775e2
Alihyder0 <162336147+Alihyder0@users.noreply.github.com>
2025-11-01 21:44:13 +0500

    Add signup role selection screen with animations

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -4,6 +4,8 @@ import '../modules/login/login_binding.dart';
 import '../modules/login/login_view.dart';
 import '../modules/onboarding/onboarding_binding.dart';
 import '../modules/onboarding/onboarding_view.dart';
+import '../modules/signup_role/signup_role_binding.dart';
+import '../modules/signup_role/signup_role_view.dart';
 import 'app_routes.dart';
 
 class AppPages {
@@ -17,6 +19,11 @@ class AppPages {
       name: AppRoutes.login,
       page: () => const LoginView(),
       binding: LoginBinding(),
+    ),
+    GetPage(
+      name: AppRoutes.signupRole,
+      page: () => const SignupRoleView(),
+      binding: SignupRoleBinding(),
     ),
   ];
 }
